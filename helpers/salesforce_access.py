0159f46aa997aceec753f17f24001f3bc27244f4
raphael@offshorly.com <raphael@offshorly.com>
2024-05-09 18:54:55 +0800

    fix salesforce access

--- helpers/salesforce_access.py
+++ helpers/salesforce_access.py
@@ -12,12 +12,19 @@ sf = Salesforce(username=username, password=password, security_token=security_to
 
 def find_user_via_opportunity_id(opportunity_id):
     query = f"""
-    SELECT AccountId.FCM_Token__c
+    SELECT AccountId
     FROM Opportunity
     WHERE Id = '{opportunity_id}'
     """
     result = sf.query(query)
-    fcm_token = result['records'][0]['Account']['FCM_Token__c']
+    account_id = result['records'][0]['AccountId']
+    account_query = f"""
+    SELECT FCM_Token__c
+    FROM Account 
+    WHERE ID = '{account_id}'
+    """
+    result = sf.query(account_query)
+    fcm_token = result['records'][0]['FCM_Token__c']
     return fcm_token
 
 def create_payment_history(opportunity_id, method_name, provider_name, total_amount, transaction_id, status):
@@ -36,12 +43,12 @@ def create_payment_history(opportunity_id, method_name, provider_name, total_amo
 
         # Data to be inserted into Payment_History__c
         payment_data = {
-            'merchantOrderId__c': opportunity_id,
+            'Merchant_Order_ID__c': opportunity_id,
             'Method_Name__c': method_name,
             'Provider_Name__c': provider_name,
             'Total_Amount_Paid__c': total_amount,
             'Dinger_Transaction_ID__c': transaction_id,
-            'Dinger Status': status,
+            'Dinger_Status__c': status,
             'CurrencyIsoCode': 'MMK',
             'Account__c': account_id  # Assuming AccountId__c is the relationship field on Payment_History__c
         }
